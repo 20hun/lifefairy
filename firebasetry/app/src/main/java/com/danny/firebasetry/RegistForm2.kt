9ae3dce9c7da3f5d971c40294398928830134e8e
danny <danny@akuo.ai>
2021-09-05 22:46:44 +0900

    apipractice

---
+++ firebasetry/app/src/main/java/com/danny/firebasetry/RegistForm2.kt
@@ -0,0 +1,9 @@
+package com.danny.firebasetry
+
+data class RegistForm2(
+    val access_token: String,
+    val message: String,
+    val name: String,
+    val refresh_token: String,
+    val userId: Int
+)
