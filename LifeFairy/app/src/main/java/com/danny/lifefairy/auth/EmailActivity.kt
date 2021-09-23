ecab1ef88ea0b83420fd41cccaba6963173b1011
danny <danny@akuo.ai>
2021-09-23 20:47:30 +0900

    email_input_modify

--- LifeFairy/app/src/main/java/com/danny/lifefairy/auth/EmailActivity.kt
+++ LifeFairy/app/src/main/java/com/danny/lifefairy/auth/EmailActivity.kt
@@ -3,10 +3,15 @@ package com.danny.lifefairy.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.danny.lifefairy.R
+import kotlinx.android.synthetic.main.activity_email.*
 
 class EmailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_email)
+
+        backBtn.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- LifeFairy/app/src/main/java/com/danny/lifefairy/auth/IntroActivity.kt
+++ LifeFairy/app/src/main/java/com/danny/lifefairy/auth/IntroActivity.kt
@@ -3,6 +3,8 @@ package com.danny.lifefairy.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.ImageButton
 import android.widget.Toast
 import com.danny.lifefairy.MainActivity
@@ -13,14 +15,32 @@ import com.kakao.sdk.user.UserApiClient
 import kotlinx.android.synthetic.main.activity_intro.*
 
 class IntroActivity : AppCompatActivity() {
+
+    private var doubleBackToExit = false
+    // 뒤로가기 메소드 onBackPressed() 재정의
+    override fun onBackPressed() {
+        if (doubleBackToExit) {
+            finishAffinity()
+        } else {
+            Toast.makeText(this, "종료하시려면 뒤로가기를 한번 더 눌러주세요.", Toast.LENGTH_SHORT).show()
+            doubleBackToExit = true
+            runDelayed(1500L) {
+                doubleBackToExit = false
+            }
+        }
+    }
+
+    fun runDelayed(millis: Long, function: () -> Unit) {
+        Handler(Looper.getMainLooper()).postDelayed(function, millis)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
         emailLoginBtn.setOnClickListener {
             val intent = Intent(this, EmailActivity::class.java)
-            startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
-            finish()
+            startActivity(intent)
         }
 
         // 로그인 정보 확인
