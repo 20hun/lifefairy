5a34bff96bed9d3766a184f9cc3394cdd31092fb
danny <danny@akuo.ai>
2021-09-27 10:10:40 +0900

    google_login

--- LifeFairy/app/src/main/java/com/danny/lifefairy/auth/IntroActivity.kt
+++ LifeFairy/app/src/main/java/com/danny/lifefairy/auth/IntroActivity.kt
@@ -9,6 +9,14 @@ import android.widget.ImageButton
 import android.widget.Toast
 import com.danny.lifefairy.MainActivity
 import com.danny.lifefairy.R
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.AuthErrorCause
 import com.kakao.sdk.user.UserApiClient
@@ -17,6 +25,11 @@ import kotlinx.android.synthetic.main.activity_intro.*
 class IntroActivity : AppCompatActivity() {
 
     private var doubleBackToExit = false
+
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
+    var auth : FirebaseAuth? = null
+
     // 뒤로가기 메소드 onBackPressed() 재정의
     override fun onBackPressed() {
         if (doubleBackToExit) {
@@ -27,6 +40,39 @@ class IntroActivity : AppCompatActivity() {
             runDelayed(1500L) {
                 doubleBackToExit = false
             }
+        }
+    }
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result.isSuccess){
+                var account = result.signInAccount
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                task ->
+                if(task.isSuccessful){
+                    moveMainPage(task.result.user)
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+
+    }
+    fun moveMainPage(user:FirebaseUser?){
+        if(user != null){
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 
@@ -42,6 +88,17 @@ class IntroActivity : AppCompatActivity() {
             val intent = Intent(this, EmailActivity::class.java)
             startActivity(intent)
         }
+
+        google_login_button.setOnClickListener {
+            googleLogin()
+        }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("353438213098-bsgj2bf2t3vln0hrulj3d783qouk3994.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         // 로그인 정보 확인
         UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
