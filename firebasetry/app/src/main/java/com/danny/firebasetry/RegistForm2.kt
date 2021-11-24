211ebe8397c9e0779f94d1ea40cc97ed41362a76
danny <danny@akuo.ai>
2021-11-24 11:42:09 +0900

    untrack_folder

--- firebasetry/app/src/main/java/com/danny/firebasetry/RegistForm2.kt
+++
@@ -1,9 +0,0 @@
-package com.danny.firebasetry
-
-data class RegistForm2(
-    val access_token: String,
-    val message: String,
-    val name: String,
-    val refresh_token: String,
-    val userId: Int
-)
